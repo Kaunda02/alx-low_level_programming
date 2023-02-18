c0be75e22061b7fdff8638e3ed5fb09a7aac675b
root <kaundabrian02@gmail.com>
2023-02-18 12:31:20 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main . A program that prints a line with puts function
+ * Return:0 (Success)
+ */
+int main(void)
+{puts("\"programming is like bulding a multilli
+ngual puzzel"")
+return(0);
+}
+
+	
+	
