c66dea41e45ffe7cfe62f8b9d89fb21735147470
root <kaundabrian02@gmail.com>
2023-03-06 08:38:48 -0800

    tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - add two members
+ * @a: the first number
+ * @b: the addition of both
+ * Return: the addition of both
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
